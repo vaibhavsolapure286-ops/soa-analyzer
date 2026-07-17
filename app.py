e576c2fa3c68d98c1911ce011cafad6b808dfd14
vaibhavsolapure286-ops <vaibhavsolapure286@gmail.com>
2026-07-17 17:38:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,61 +1,14 @@
-cleared_bounce_count = 0
-pending_bounce_count = 0
-
 bounce_details = []
 
-for bounce in bounces:
-
-    recovered = False
-
-    bounce_date = datetime.strptime(
-        bounce["date"],
-        "%d-%b-%y"
+for i, bounce in enumerate(bounces, start=1):
+    bounce_details.append(
+        f"""
+{i}.
+   Bounce Date      : {bounce['bounce_date']}
+   Bounce Amount    : ₹{bounce['amount']}
+   Cleared          : {bounce['cleared']}
+   Cleared On       : {bounce['cleared_on']}
+   Delay            : {bounce['delay']}
+   Bounce Charges   : {bounce['charges_status']}
+"""
     )
-
-    for r in receipts:
-
-        receipt_date = datetime.strptime(
-            r["date"],
-            "%d-%b-%y"
-        )
-
-        if receipt_date > bounce_date:
-
-            dpd_days = (
-                receipt_date -
-                bounce_date
-            ).days
-
-            dpd_months = round(
-                dpd_days / 30,
-                1
-            )
-
-            cleared_bounce_count += 1
-
-            recovered = True
-
-            bounce_details.append(
-                f"""
-Bounce Date : {bounce['date']}
-Bounce Amount : ₹{bounce['amount']:,.0f}
-Recovered On : {r['date']}
-Recovery Amount : ₹{r['amount']:,.0f}
-DPD : {dpd_days} Days ({dpd_months} Months)
-Status : Cleared
-"""
-            )
-
-            break
-
-    if not recovered:
-
-        pending_bounce_count += 1
-
-        bounce_details.append(
-            f"""
-Bounce Date : {bounce['date']}
-Bounce Amount : ₹{bounce['amount']:,.0f}
-Status : Pending
-"""
-        )
