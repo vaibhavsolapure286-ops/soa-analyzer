8620c90269096887f3cc6b9c40811d5ce60a8d9f
vaibhavsolapure286-ops <vaibhavsolapure286@gmail.com>
2026-07-17 19:44:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,6 +42,7 @@ for receipt in receipts:
 
             late_emi_details.append(
                 f"""
+                
 EMI Due Date : {due_date.strftime('%d-%b-%Y')}
 Payment Date : {payment_date.strftime('%d-%b-%Y')}
 Delay : {dpd_days} Days ({dpd_months} Months)
